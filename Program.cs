3b802c01c1aa91de77e65223ae22605e20afa10c
iFaalK <illia.falkovych@nure.ua>
2024-12-22 20:56:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingletonWork;
+
+namespace UserSingleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton1 = Singleton.Instance;
+
+            singleton1.City = "Kyiv";
+            singleton1.Date = 120;
+            singleton1.Latitude = 50.45;
+            singleton1.LatitudeLocation = "N";
+
+            string season = singleton1.DetermineSeason();
+
+            Console.WriteLine("\n=== Results ===");
+            Console.WriteLine($"City: {singleton1.City}");
+            Console.WriteLine($"Date (day of the year): {singleton1.Date}");
+            Console.WriteLine($"Latitude: {singleton1.Latitude}° {singleton1.LatitudeLocation}");
+            Console.WriteLine($"Season: {season}");
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonWork
+{
+    public sealed class Singleton
+    {
+        private int _date;
+        private double _latitude;
+        private string _latitudeLocation;
+        private string _city;
+
+        public int Date
+        {
+            get => _date;
+            set
+            {
+                if (value < 1 || value > 365)
+                {
+                    throw new ArgumentOutOfRangeException("Out of range (1;365)");
+                }
+                _date = value;
+            }
+        }
+
+        public double Latitude
+        {
+            get => _latitude;
+            set
+            {
+                if (value < 0 || value > 90) throw new ArgumentOutOfRangeException("Out of range(0;90)");
+                _latitude = value;
+            }
+        }
+
+        public string LatitudeLocation
+        {
+            get => _latitudeLocation;
+            set
+            {
+                if (value != "N" && value != "S")
+                {
+                    throw new ArgumentException("Invalid location.");
+                }
+                _latitudeLocation = value;
+            }
+        }
+
+        public string City
+        {
+            get => _city;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Name cannot be null");
+                }
+                _city = value;
+            }
+        }
+
+        private static readonly Lazy<Singleton> _instance = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance => _instance.Value;
+
+        private Singleton() { }
+
+        public string DetermineSeason()
+        {
+            if (LatitudeLocation == "N")
+            {
+                if (Date < 60 || Date > 334) return "Winter";
+                if (Date < 152 && Date > 59) return "Spring";
+                if (Date < 245 && Date > 151) return "Summer";
+                if (Date < 335 && Date > 244) return "Autumn";
+                return null;
+            }
+            else
+            {
+                if (Date < 60 || Date > 334) return "Summer";
+                if (Date < 152 && Date > 59) return "Autumn";
+                if (Date < 245 && Date > 151) return "Winter";
+                if (Date < 335 && Date > 244) return "Spring";
+                return null;
+            }
+        }
+    }
+}
